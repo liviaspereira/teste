910e4c83d07d2267e4c4437e788d1a847d949d2d
Livia Pereira <liviaspereira@hotmail.com>
2022-08-22 09:36:23 -0300

    Arruma teste

--- codigo.py
+++ codigo.py
@@ -1,5 +1,5 @@
 def hello_world(request):
     request_json = request.get_json()
     if "batata" in request_json["data"]:
-        return "ok", 202
+        return "ok", 203
     return "Bad request - Not batata", 400
